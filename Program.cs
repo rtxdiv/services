6255840076a2030b7d67bcb01e3b9124cce541d1
rtxdiv <mitrushonkovdima@gmail.com>
2026-01-24 11:07:46 +0300

    [jan24-1] middleware for checking existence of service image

--- Program.cs
+++ Program.cs
@@ -18,29 +18,40 @@ if (string.IsNullOrEmpty(adminKey)) {
     throw new InvalidOperationException("Set ADMIN_KEY={value} in .env.secret");
 }
 
-// регистрация сервисов
 builder.Services.AddScoped<IRootService, RootService>();
 builder.Services.AddScoped<IRequestsService, RequestsService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IEditorService, EditorService>();
 
-// регистрация сервисов-фильтов
 builder.Services.AddScoped<RequireAdminFilter>();
 builder.Services.AddScoped<CheckAdminFilter>();
 
-// внедрение конфигурации в сервис
 builder.Services.AddScoped<IAuthService>(provider => {
     AppDbContext context = provider.GetRequiredService<AppDbContext>();
     return new AuthService(context, adminKey);
 });
 
-// контроллеры
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-app.UseStaticFiles();
 app.UseRouting();
 app.MapControllers();
+
+app.Use(async (context, next) => {
+    if (context.Request.Path.StartsWithSegments("/img/services") && context.Request.Path.Value != null) {
+
+        string filePath = Path.Combine("wwwroot", context.Request.Path.Value.TrimStart('/'));
+        if (!File.Exists(filePath)) {
+            await context.Response.SendFileAsync(Path.Combine("wwwroot", "img", "noimg.png"));
+        } else {
+            await next();
+        }
+
+    } else {
+        await next();
+    }
+});
+
+app.UseStaticFiles();
 
 app.Run();
